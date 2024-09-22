6f2c080758d5cc60624496b013b2b74e45ce4f8d
Ubaid Ullah <156952534+ubaid164@users.noreply.github.com>
2024-09-22 09:50:06 +0500

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,23 +1,22 @@
 # config/config.py
 
-TELEGRAM_BOT_TOKEN = '7324744926:AAFG9f_Nj7goEORkrEUR3XlXz7Ky_LofaHY'
+TELEGRAM_BOT_TOKEN = '7324lXz7Ky_LofaHY'
 
-TELEGRAM_API_ID = '26087037'  # Your real API ID here
-TELEGRAM_API_HASH = 'c9132b5d5ade1212e7d7dabe2d9680e7'  # Your real API hash here
+TELEGRAM_API_ID = '26'  # Your real API ID here
+TELEGRAM_API_HASH = '0e7'  # Your real API hash here
 
 # -----------------------------
 # Twitter API Credentials
 # -----------------------------
-TWITTER_API_KEY = 'VVm3MlJDhthjG2i4DquHOvIn7'
-TWITTER_API_SECRET_KEY = 'uY5D46o2gqHqpgyOm3FKBVBOQMuXvsAktmoKvRd8yFVHQWNcVv'
-TWITTER_ACCESS_TOKEN = '1834400789328003072-kBPjQRREoZEG8OidGb9Z7tnGoeEh7N'
-TWITTER_ACCESS_TOKEN_SECRET = 'hvgTZ6T9z2Fb20pe4ZMT6C5PVOwQgBtmZLdgjlZNuFBe3'
+TWITTER_API_KEY = 'VVm37'
+TWITTER_API_SECRET_KEY = 'uY5DNcVv'
+TWITTER_ACCESS_TOKEN = ''
+TWITTER_ACCESS_TOKEN_SECRET = 'hvgTrepljlZNuFBe3'
 
 # -----------------------------
 # Twitter Bearer Token (for API v2)
 # -----------------------------
-TWITTER_BEARER_TOKEN = 'AAAAAAAAAAAAAAAAAAAAAHt0vwEAAAAA3VXR9%2Br2fgT9gcfy4dO%2FHxiS0xc%3DutasY9gXxrxTTs3Kz1mtsBvnjyrZtnxqbfBhkwKAficywibnQj'  # Use this for Twitter API v2
-
+TWITTER_BEARER_TOKEN = '
 # -----------------------------
 # Cryptocurrency Wallet Addresses
 # -----------------------------
@@ -25,20 +24,7 @@ BTC_WALLET = 'your_btc_wallet_address'  # Replace with your Bitcoin wallet addre
 ETH_WALLET = 'your_eth_wallet_address'  # Replace with your Ethereum wallet address
 SOL_WALLET = 'your_sol_wallet_address'  # Replace with your Solana wallet address
 
-# -----------------------------
-# Coupon Codes for Discounts
-# -----------------------------
-COUPON_CODES = {
-    "FREEAD2024": 100,  # 100% discount coupon for free ads
-    "DISCOUNT50": 50,   # 50% discount coupon
-}
 
-# -----------------------------
-# Telegram Bot Owner ID (your Telegram User ID)
-# -----------------------------
 BOT_OWNER_ID = -1002219808114  # Replace this with YOUR actual Telegram User ID
 
 # -----------------------------
-# Telegram Settings (General)
-# -----------------------------
-RAID_MESSAGE_TEMPLATE = "Perform these actions on the tweet: {tweet_link}"  # Template for raid message
